234e76ef9160c3fae5a25ec64dc3733234cbff42
Leundo <daysundoing@outlook.com>
2023-07-07 12:35:09 +0800

    Initial Commit

---
+++ Squib/Squib.h
@@ -0,0 +1,18 @@
+//
+//  Squib.h
+//  Squib
+//
+//  Created by Undo Hatsune on 2023/07/07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Squib.
+FOUNDATION_EXPORT double SquibVersionNumber;
+
+//! Project version string for Squib.
+FOUNDATION_EXPORT const unsigned char SquibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Squib/PublicHeader.h>
+
+
